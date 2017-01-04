bc16b4e13b87cb6f62ef51b8444abf040dff277b
guyucortez <guyucortez@hotmail.com>
2017-01-04 12:41:53 -0300

    Se unifica

--- GeHos/GeHos/Controllers/HomeController.cs
+++ GeHos/GeHos/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace GeHos.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {            
+        {
             return View();
         }
 
@@ -39,83 +39,5 @@ namespace GeHos.Controllers
             return PartialView();
         }
 
-        #region Datos Estaticos Menu        
-        private List<mnuEntitie> GetMenu(string name)
-        {
-            List<mnuEntitie> mnus = new List<mnuEntitie> {
-                // /GestionHospitalaria/
-                new mnuEntitie {
-                    mnuId = 1,
-                    mnuIdPadre=null,
-                    mnuNombre="GestionHospitalaria",
-                    mnuImagen="",
-                    mnuAccion="",
-                    mnuController=""
-                },
-
-                // /GestionHospitalaria/Turnos
-                new mnuEntitie {
-                    mnuId = 2,
-                    mnuIdPadre=1,
-                    mnuNombre="Turnos",
-                    mnuImagen="",
-                    mnuAccion="",
-                    mnuController=""
-                },
-
-                //*/GestionHospitalaria/Turnos/Agenda
-                new mnuEntitie {
-                    mnuId = 3,
-                    mnuIdPadre=2,
-                    mnuNombre="Agenda",
-                    mnuImagen="",
-                    mnuAccion="Index",
-                    mnuController="Agenda"
-                },
-                
-                //*/GestionHospitalaria/Turnos/BloqueosMasivos
-                new mnuEntitie {
-                    mnuId = 4,
-                    mnuIdPadre=2,
-                    mnuNombre="Bloqueos Masivos",
-                    mnuImagen="",
-                    mnuAccion="",
-                    mnuController="catBloqueosMasivos"
-                },
-                
-                //*/GestionHospitalaria/Turnos/CentroDeSaludPublicidad
-                new mnuEntitie {
-                    mnuId = 5,
-                    mnuIdPadre=2,
-                    mnuNombre="Centro de Salud Publicidad",
-                    mnuImagen="",
-                    mnuAccion="",
-                    mnuController="catCentroDeSaludPublicidad"
-                },
-                
-                //* /GestionHospitalaria/Turnos/Turnos
-                new mnuEntitie {
-                    mnuId = 6,
-                    mnuIdPadre=2,
-                    mnuNombre="Turnos",
-                    mnuImagen="",
-                    mnuAccion="Index",
-                    mnuController="catTurnos"
-                },
-                
-                //*/GestionHospitalaria/Turnos/VisualizadorDeTurnos
-                new mnuEntitie {
-                    mnuId = 7,
-                    mnuIdPadre=2,
-                    mnuNombre="Visualizador de Turnos",
-                    mnuImagen="",
-                    mnuAccion="",
-                    mnuController="catVisualizadorDeTurnos"
-                },
-
-
-
-
-       
     }
 }
