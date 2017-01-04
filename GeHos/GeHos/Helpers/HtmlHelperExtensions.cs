240ee75ff6e8e7d1345709104c86ab4de0c885d5
jpgarcia89 <jpgarcia89@hotmail.com>
2017-01-04 12:38:18 -0300

    Menus del Sistema
    Se agrega la funcionalidad de lectura de menus desde la Base de Datos

--- GeHos/GeHos/Helpers/HtmlHelperExtensions.cs
+++ GeHos/GeHos/Helpers/HtmlHelperExtensions.cs
@@ -2,44 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using GeHos.Controllers;
+using GeHosContract.Contrato;
 using System.Web.Mvc;
 
 namespace GeHos.Helpers
 {
     public static class HtmlHelperExtensions
     {
-        public static MvcHtmlString RenderMenu(this HtmlHelper helper, List<HomeController.mnuEntitie> menus)
+        public static MvcHtmlString RenderMenu(this HtmlHelper helper, List<MenuVM> menus)
         {            
-            var menusPadres = menus == null ? null : menus.Where(r => r.mnuIdPadre == null).ToList();
+            var menusPadres = menus == null ? null : menus.Where(r => r.PadreID == null).ToList();
             return MvcHtmlString.Create(createMenu(menusPadres, menus));
         }
 
-        private static string createMenu(List<HomeController.mnuEntitie> menusPadres, List<HomeController.mnuEntitie> menusAll, string output = null)
+        private static string createMenu(List<MenuVM> menusPadres, List<MenuVM> menusAll, string output = null)
         {
             if (menusPadres != null && menusAll != null)
             {
 
-                if (menusPadres.Any(r => r.mnuIdPadre != null))//Son opciones de menu q tienen un padre
+                if (menusPadres.Any(r => r.PadreID != null))//Son opciones de menu q tienen un padre
                 {
                     foreach (var item in menusPadres)
                     {
 
-                        if (menusAll.Where(r => r.mnuIdPadre == item.mnuId).Any())
+                        if (menusAll.Where(r => r.PadreID == item.ID).Any())
                         {
-                            string a = string.Format(@"<li><a href=""#""><i class=""fa fa-circle-o""></i> {0} <span class=""pull-right-container""><i class=""fa fa-angle-left pull-right""></i></span></a><ul class=""treeview-menu"">", item.mnuNombre);
+                            string a = string.Format(@"<li><a href=""#""><i class=""fa fa-circle-o""></i> {0} <span class=""pull-right-container""><i class=""fa fa-angle-left pull-right""></i></span></a><ul class=""treeview-menu"">", item.Nombre);
                             output += a;
 
-                            output = createMenu(menusAll.Where(r => r.mnuIdPadre == item.mnuId).ToList(), menusAll, output);
+                            output = createMenu(menusAll.Where(r => r.PadreID == item.ID).ToList(), menusAll, output);
 
                             string b = @"</ul></li>";
                             output += b;
                         }
                         else
                         {
-                            string accion = string.IsNullOrEmpty(item.mnuAccion) ? "" : item.mnuAccion;
-                            string url = "/" + item.mnuController + "/" + accion;
-                            string c = string.Format(@"<li><a href=""{1}""><i class=""fa fa-circle-o""></i> {0} </a></li>", item.mnuNombre, url);
+                            string accion = string.IsNullOrEmpty(item.Accion) ? "" : item.Accion;
+                            string url = "/" + item.Controlador + "/" + (accion=="Index"?String.Empty:accion);
+                            string c = string.Format(@"<li><a href=""{1}""><i class=""{2}""></i> {0} </a></li>", item.Nombre, url,item.Icono);
                             output += c;
                         }
                     }
@@ -50,15 +50,15 @@ namespace GeHos.Helpers
 
                     foreach (var item in menusPadres)
                     {
-                        string x = string.Format(@"<li class=""treeview""><a href = ""#"" ><i class=""fa fa-circle-o""></i><span> {0} </span><span class=""pull-right-container""><i class=""fa fa-angle-left pull-right""></i></span></a>", item.mnuNombre);
+                        string x = string.Format(@"<li class=""treeview""><a href = ""#"" ><i class=""fa fa-circle-o""></i><span> {0} </span><span class=""pull-right-container""><i class=""fa fa-angle-left pull-right""></i></span></a>", item.Nombre);
                         output += x;
 
-                        if (menusAll.Where(r => r.mnuIdPadre == item.mnuId).Any())
+                        if (menusAll.Where(r => r.PadreID == item.ID).Any())
                         {
                             string y = @"<ul class=""treeview-menu"">";
                             output += y;
 
-                            output = createMenu(menusAll.Where(r => r.mnuIdPadre == item.mnuId).ToList(), menusAll, output);
+                            output = createMenu(menusAll.Where(r => r.PadreID == item.ID).ToList(), menusAll, output);
 
                             string z = @"</ul>";
                             output += z;
