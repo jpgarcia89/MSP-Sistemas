e04e281f0a21c9f5f1922a85f6169e3c044925f5
guyucortez <guyucortez@hotmail.com>
2017-06-22 10:31:28 -0300

    Algo

---
+++ MSP-RegProf/MSP-RegProf/MSP/Models/Seguridad/AspNetUsersVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MSP_RegProf.Models.Seguridad
+{
+    [MetadataType(typeof(PersonaMetadata))]
+    public partial class AspNetUsers
+    {
+    }
+
+    public class AspNetUsersMetadata
+    {
+
+    }
+}
